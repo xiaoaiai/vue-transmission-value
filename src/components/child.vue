2ac4519a803a47cef0cfde5bac44c12a7b71e584
liuqiaoai <fightingcoder@163.com>
2020-11-13 17:28:05 +0800

    父子组件传值

---
+++ src/components/child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <div>父组件传过来的值：<span>{{parentVal}}</span></div>
+    <button @click="transferValByEmit">通过$emit传值</button>
+    <button @click="transferValByParent">通过$parent传值</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'child',
+  props: {
+    parentVal: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      childVal: 'childVal'
+    }
+  },
+  methods: {
+    transferValByEmit () {
+      this.$emit('toParent', '$emit' + this.childVal)
+    },
+    transferValByParent () {
+      this.$parent.exceptChild('$parent' + this.childVal)
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/views/parent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <Child :parentVal="toChild" @toParent="exceptChild"></Child>
+    <div>子组件传过来的值：<span>{{childValue}}</span></div>
+  </div>
+</template>
+<script>
+import Child from '@/components/child'
+export default {
+  name: 'parent',
+  components: {
+    Child
+  },
+  data () {
+    return {
+      toChild: 'parentVal',
+      childValue: ''
+    }
+  },
+  methods: {
+    exceptChild (childValue) {
+      this.childValue = childValue
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
